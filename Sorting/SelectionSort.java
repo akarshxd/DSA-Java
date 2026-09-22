bd82382f3e3c5a195085311b7d941f1809c9c310
Akarsh <aakarshjaysingh69@gmail.com>
2026-09-22 12:35:59 +0530

    updated

--- Sorting/SelectionSort.java
+++ Sorting/SelectionSort.java
@@ -32,7 +32,19 @@ class SelectionSort{
     }
 
     static void selection(int [] arr){
-        
+        int n = arr.length;
+        for(int i =0;i <n-1; i++){
+            int min = Integer.MAX_VALUE, mindex = -1;
+            for(int j = i ; j<n; j++){
+                if(arr[j]<min){
+                    min = arr[j];
+                    mindex = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i]= arr[mindex];
+            arr[mindex] = temp;
+        }
     }
     
     public static void main(String[] args) {
